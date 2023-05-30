3fe544a1419c3b16e2abb4cde13bb0fd52dc23a8
Danamarie10 <dana.patrick10@outlook.com>
2023-05-30 11:38:53 -0400

    finished exercise 2

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public static class DataAccessFactory
+    {
+        public static IDataAccess GetDatabase(string dataBase)
+        {
+            switch(dataBase.ToLower())
+            {
+                case "sql":
+                    return new SQLDataAccess();
+                case "mongo":
+                    return new MongoDataAccess();
+                case "listdata":
+                    return new ListDataAccess();
+                default:
+                    return new ListDataAccess();
+            }
+        }
+    }
+}
